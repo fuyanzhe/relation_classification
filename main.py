37dcdd7c991e3a5e506ce9a2bbf59e3c04c3a90a
han <hxy8149989@163.com>
2017-08-11 00:01:55 +0800

    cnn test

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 # -*- encoding: utf-8 -*-
 # Created by han on 17-7-8
+
 from models import *
 from data_loader import DataLoader
 
 
-class cnn_setting(object):
-    pass
-
-
-class rnn_setting(object):
-    pass
-
-
 def main():
     data_loader = DataLoader('./data', multi_ins=False)
-    batches = data_loader.get_train_batches(batch_size=1000)
-    for batch in batches:
-        print len(batch), len(batch['word']), len(batch['pos1']), len(batch['pos2']), len(batch['len']), len(batch['y'])
-        print batch['word'][0]
-        print batch['pos1'][0]
-        print batch['pos2'][0]
-        print batch['len'][0]
-        print batch['y'][0]
-    pass
 
-    with tf.Graph().as_default():
-        sess = tf.Session()
-        with sess.as_default():
-            initializer = tf.contrib.layers.xavier_initializer()
+    cnn_setting = CNNSetting()
+    cnn_model = CNN(data_loader.wordembedding, cnn_setting)
+    optimizer = cnn_model.get_optimizer()
+
+    print cnn_model.model_name
+
+    with tf.Session() as session:
+        tf.global_variables_initializer().run()
+        cnn_s_feed_test = {cnn_model.input_words: data_loader.test_word,
+                              cnn_model.input_pos1: data_loader.test_pos1,
+                              cnn_model.input_pos2: data_loader.test_pos2,
+                              cnn_model.input_labels: data_loader.test_y,
+                              cnn_model.dropout_keep_prob: 1}
+        for i in range(100):
+            j = 0
+            batches = data_loader.get_train_batches(batch_size=512)
+            for batch in batches:
+                j += 1
+                cnn_s_dict = {cnn_model.input_words: batch['word'],
+                              cnn_model.input_pos1: batch['pos1'],
+                              cnn_model.input_pos2: batch['pos2'],
+                              cnn_model.input_labels: batch['y'],
+                              cnn_model.dropout_keep_prob: 0.5
+                              }
+                session.run(optimizer, feed_dict=cnn_s_dict)
+                loss = session.run(cnn_model.model_loss, feed_dict=cnn_s_dict)
+                if j % 100 == 0:
+                    print i, j, loss
+            test_loss = session.run(cnn_model.model_loss, feed_dict=cnn_s_feed_test)
+            print i, test_loss
+
+
 
 
 if __name__ == '__main__':
