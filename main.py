311619aa39a0ad359b35540d9d234133dfbe131d
han <hxy8149989@163.com>
2017-08-14 19:59:42 +0800

    add rnn, birnn

--- main.py
+++ main.py
@@ -8,10 +8,75 @@ from evaluate import *
 
 def main():
     data_loader = DataLoader('./data', multi_ins=False)
-    cnn_setting = CNNSetting()
-    cnn_model = CNN(data_loader.wordembedding, cnn_setting)
-    print cnn_model.model_name
 
+    # # cnn test
+    # cnn_setting = CNNSetting()
+    # cnn_model = CNN(data_loader.wordembedding, cnn_setting)
+    # print cnn_model.model_name
+    #
+    # with tf.Session() as session:
+    #     tf.global_variables_initializer().run()
+    #     test_data = data_loader.get_test_data()
+    #     for i in range(100):
+    #         j = 0
+    #         batches = data_loader.get_train_batches(batch_size=1024)
+    #         for batch in batches:
+    #             j += 1
+    #             loss = cnn_model.fit(session, batch, dropout_keep_rate=0.5)
+    #             _, c_label = cnn_model.predict(session, batch)
+    #             if j % 100 == 0:
+    #                 p, r, f1 = get_p_r_f1(c_label, batch.y)
+    #                 print 'epoch: {:>3}, batch: {:>4}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+    #                     i, j, loss, p * 100, r * 100, f1 * 100
+    #                 )
+    #         test_loss, test_pred = cnn_model.predict(session, test_data)
+    #         p, r, f1 = get_p_r_f1(test_pred, test_data.y)
+    #         print 'epoch: {:>3}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+    #             i, test_loss, p * 100, r * 100, f1 * 100
+    #         )
+
+    # # rnn test
+    # lstm_setting = LSTMSetting()
+    # lstm_model = LSTM(data_loader.wordembedding, lstm_setting)
+    #
+    # with tf.Session() as session:
+    #     tf.global_variables_initializer().run()
+    #     test_data = data_loader.get_test_data()
+    #     for i in range(100):
+    #         j = 0
+    #         batches = data_loader.get_train_batches(batch_size=1024)
+    #         for batch in batches:
+    #             j += 1
+    #             loss = lstm_model.fit(session, batch, dropout_keep_rate=0.5)
+    #             _, c_label = lstm_model.predict(session, batch)
+    #             if j % 100 == 0:
+    #                 p, r, f1 = get_p_r_f1(c_label, batch.y)
+    #                 print 'epoch: {:>3}, batch: {:>4}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+    #                     i, j, loss, p * 100, r * 100, f1 * 100
+    #                 )
+    #         test_loss, test_pred = lstm_model.predict(session, test_data)
+    #         p, r, f1 = get_p_r_f1(test_pred, test_data.y)
+    #         print 'epoch: {:>3}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+    #             i, test_loss, p * 100, r * 100, f1 * 100
+    #         )
+
+    # bigru test
+    bigru_setting = BiGRUSetting()
+    bigru_model = BiGRU(data_loader.wordembedding, bigru_setting)
+
+    # with tf.Session() as session:
+    #     tf.global_variables_initializer().run()
+    #     for i in range(100):
+    #         batches = data_loader.get_train_batches(batch_size=128)
+    #         for batch in batches:
+    #             feed_dict = {bigru_model.input_words: batch.word,
+    #                          bigru_model.input_pos1: batch.pos1,
+    #                          bigru_model.input_pos2: batch.pos2,
+    #                          bigru_model.input_labels: batch.y,
+    #                          bigru_model.dropout_keep_rate: 0.5
+    #                          }
+    #             op, opf, opb = session.run([bigru_model.outputs], feed_dict=feed_dict)
+    #             pass
     with tf.Session() as session:
         tf.global_variables_initializer().run()
         test_data = data_loader.get_test_data()
@@ -20,15 +85,18 @@ def main():
             batches = data_loader.get_train_batches(batch_size=1024)
             for batch in batches:
                 j += 1
-                loss = cnn_model.fit(session, batch, dropout_keep_rate=0.5)
-                _, c_label = cnn_model.predict(session, batch)
+                loss = bigru_model.fit(session, batch, dropout_keep_rate=0.5)
+                _, c_label = bigru_model.predict(session, batch)
                 if j % 100 == 0:
                     p, r, f1 = get_p_r_f1(c_label, batch.y)
-                    print i, j, loss, p, r, f1
-            test_loss, test_pred = cnn_model.predict(session, test_data)
+                    print 'epoch: {:>3}, batch: {:>4}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+                        i, j, loss, p * 100, r * 100, f1 * 100
+                    )
+            test_loss, test_pred = bigru_model.predict(session, test_data)
             p, r, f1 = get_p_r_f1(test_pred, test_data.y)
-            print i, test_loss, p, r, f1
-
+            print 'epoch: {:>3}, lost: {:.3f}, p: {:.3f}%, r: {:.3f}%, f1:{:.3f}%'.format(
+                i, test_loss, p * 100, r * 100, f1 * 100
+            )
 
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -6,11 +6,33 @@ import tensorflow as tf
 class CNNSetting(object):
     def __init__(self):
         self.pos_num = 200
-        self.pos_size = 50
+        self.pos_size = 5
         self.class_num = 31
         self.sent_len = 100
         self.filter_sizes = [3]
         self.filter_num = 200
+        self.learning_rate = None
+
+
+class LSTMSetting(object):
+    def __init__(self):
+        self.pos_num = 200
+        self.pos_size = 5
+        self.class_num = 31
+        self.sent_len = 100
+        self.hidden_size = 100
+        self.layers = 1
+        self.learning_rate = None
+
+
+class BiGRUSetting(object):
+    def __init__(self):
+        self.pos_num = 200
+        self.pos_size = 5
+        self.class_num = 31
+        self.sent_len = 100
+        self.hidden_size = 100
+        self.layers = 1
         self.learning_rate = None
 
 
@@ -41,7 +63,7 @@ class CNN(object):
 
         # inputs
         self.input_words = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_words')
-        self.input_labels = tf.placeholder(tf.float32, [None, self.class_num], name='labels')
+        self.input_labels = tf.placeholder(tf.int32, [None, self.class_num], name='labels')
 
         # position feature
         self.input_pos1 = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_pos1')
@@ -129,6 +151,217 @@ class CNN(object):
         h_drop = tf.nn.dropout(h_pool_flat, self.dropout_keep_rate)
 
         return h_drop
+
+    def fit(self, session, input_data, dropout_keep_rate):
+        feed_dict = {self.input_words: input_data.word,
+                     self.input_pos1: input_data.pos1,
+                     self.input_pos2: input_data.pos2,
+                     self.input_labels: input_data.y,
+                     self.dropout_keep_rate: dropout_keep_rate
+                     }
+        session.run(self.optimizer, feed_dict=feed_dict)
+        model_loss = session.run(self.model_loss, feed_dict=feed_dict)
+        return model_loss
+
+    def predict(self, session, input_data):
+        feed_dict = {self.input_words: input_data.word,
+                     self.input_pos1: input_data.pos1,
+                     self.input_pos2: input_data.pos2,
+                     self.input_labels: input_data.y,
+                     self.dropout_keep_rate: 1}
+        model_loss, label_pred = session.run([self.model_loss, self.class_label], feed_dict=feed_dict)
+        return model_loss, label_pred
+
+
+class LSTM(object):
+    def __init__(self, word_embedding, setting):
+        # model name
+        self.model_name = 'LSTM'
+
+        # settings
+        self.max_sentence_len = setting.sent_len
+        self.hidden_size = setting.hidden_size
+        self.class_num = setting.class_num
+        self.pos_num = setting.pos_num
+        self.pos_size = setting.pos_size
+        self.learning_rate = setting.learning_rate
+
+        # embedding matrix
+        self.embed_matrix_word = tf.get_variable(
+            'embed_matrix_word', word_embedding.shape,
+            initializer=tf.constant_initializer(word_embedding)
+        )
+        self.embed_size_word = int(self.embed_matrix_word.get_shape()[1])
+        self.embed_matrix_pos1 = tf.get_variable('embed_matrix_pos1', [self.pos_num, self.pos_size])
+        self.embed_matrix_pos2 = tf.get_variable('embed_matrix_pos2', [self.pos_num, self.pos_size])
+
+        # inputs
+        self.input_words = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_words')
+        self.input_labels = tf.placeholder(tf.int32, [None, self.class_num], name='labels')
+
+        # position feature
+        self.input_pos1 = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_pos1')
+        self.input_pos2 = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_pos2')
+
+        # dropout keep probability
+        self.dropout_keep_rate = tf.placeholder(tf.float32, name="dropout_keep_rate")
+
+        # embedded
+        self.emb_word = tf.nn.embedding_lookup(self.embed_matrix_word, self.input_words)
+        self.emb_pos1 = tf.nn.embedding_lookup(self.embed_matrix_pos1, self.input_pos1)
+        self.emb_pos2 = tf.nn.embedding_lookup(self.embed_matrix_pos2, self.input_pos2)
+
+        # concat embeddings
+        self.emb_all = tf.concat([self.emb_word, self.emb_pos1, self.emb_pos2], 2)
+        self.emb_all_us = tf.unstack(self.emb_all, num=self.max_sentence_len, axis = 1)
+
+        # states and outputs
+        with tf.name_scope('sentence_encoder'):
+            # cell
+            # self.lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size, forget_bias=0.0, state_is_tuple=True)
+            self.lstm_cell = tf.nn.rnn_cell.GRUCell(self.hidden_size)
+            self.lstm_cell = tf.nn.rnn_cell.DropoutWrapper(self.lstm_cell, output_keep_prob=self.dropout_keep_rate)
+
+            # rnn
+            self.outputs, self.states = tf.contrib.rnn.static_rnn(self.lstm_cell, self.emb_all_us, dtype=tf.float32)
+
+        self.output_final = self.outputs[-1]
+
+        # softmax
+        with tf.name_scope('softmax'):
+            self.softmax_w = tf.get_variable('softmax_W', [self.hidden_size, self.class_num])
+            self.softmax_b = tf.get_variable('softmax_b', [self.class_num])
+            self.softmax_pred = tf.matmul(self.output_final, self.softmax_w) + self.softmax_b
+            self.softmax_res = tf.nn.softmax(self.softmax_pred)
+
+        # get max softmax predict result of each relation
+        self.maxres_by_rel = tf.reduce_max(self.softmax_res, 0)
+
+        # class label
+        self.class_label = tf.argmax(self.softmax_res, 1)
+
+        # choose the min loss instance index
+        self.instance_loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.softmax_pred, labels=self.input_labels)
+        self.min_loss_idx = tf.argmin(self.instance_loss, 0)
+
+        # model loss
+        self.model_loss = tf.reduce_mean(self.instance_loss)
+
+        # optimizer
+        if self.learning_rate:
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.model_loss)
+        else:
+            self.optimizer = tf.train.AdamOptimizer().minimize(self.model_loss)
+
+        # # saver
+        # self.saver = tf.train.Saver(tf.all_variables())
+
+    def fit(self, session, input_data, dropout_keep_rate):
+        feed_dict = {self.input_words: input_data.word,
+                     self.input_pos1: input_data.pos1,
+                     self.input_pos2: input_data.pos2,
+                     self.input_labels: input_data.y,
+                     self.dropout_keep_rate: dropout_keep_rate
+                     }
+        session.run(self.optimizer, feed_dict=feed_dict)
+        model_loss = session.run(self.model_loss, feed_dict=feed_dict)
+        return model_loss
+
+    def predict(self, session, input_data):
+        feed_dict = {self.input_words: input_data.word,
+                     self.input_pos1: input_data.pos1,
+                     self.input_pos2: input_data.pos2,
+                     self.input_labels: input_data.y,
+                     self.dropout_keep_rate: 1}
+        model_loss, label_pred = session.run([self.model_loss, self.class_label], feed_dict=feed_dict)
+        return model_loss, label_pred
+
+
+class BiGRU(object):
+    def __init__(self, word_embedding, setting):
+        # model name
+        self.model_name = 'LSTM'
+
+        # settings
+        self.max_sentence_len = setting.sent_len
+        self.hidden_size = setting.hidden_size
+        self.class_num = setting.class_num
+        self.pos_num = setting.pos_num
+        self.pos_size = setting.pos_size
+        self.learning_rate = setting.learning_rate
+
+        # embedding matrix
+        self.embed_matrix_word = tf.get_variable(
+            'embed_matrix_word', word_embedding.shape,
+            initializer=tf.constant_initializer(word_embedding)
+        )
+        self.embed_size_word = int(self.embed_matrix_word.get_shape()[1])
+        self.embed_matrix_pos1 = tf.get_variable('embed_matrix_pos1', [self.pos_num, self.pos_size])
+        self.embed_matrix_pos2 = tf.get_variable('embed_matrix_pos2', [self.pos_num, self.pos_size])
+
+        # inputs
+        self.input_words = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_words')
+        self.input_labels = tf.placeholder(tf.int32, [None, self.class_num], name='labels')
+
+        # position feature
+        self.input_pos1 = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_pos1')
+        self.input_pos2 = tf.placeholder(tf.int32, [None, self.max_sentence_len], name='input_pos2')
+
+        # dropout keep probability
+        self.dropout_keep_rate = tf.placeholder(tf.float32, name="dropout_keep_rate")
+
+        # embedded
+        self.emb_word = tf.nn.embedding_lookup(self.embed_matrix_word, self.input_words)
+        self.emb_pos1 = tf.nn.embedding_lookup(self.embed_matrix_pos1, self.input_pos1)
+        self.emb_pos2 = tf.nn.embedding_lookup(self.embed_matrix_pos2, self.input_pos2)
+
+        # concat embeddings
+        self.emb_all = tf.concat([self.emb_word, self.emb_pos1, self.emb_pos2], 2)
+        self.emb_all_us = tf.unstack(self.emb_all, num=self.max_sentence_len, axis = 1)
+
+        # states and outputs
+        with tf.name_scope('sentence_encoder'):
+            # cell
+            self.foward_cell = tf.nn.rnn_cell.GRUCell(self.hidden_size)
+            self.backward_cell = tf.nn.rnn_cell.GRUCell(self.hidden_size)
+            self.foward_cell = tf.nn.rnn_cell.DropoutWrapper(self.foward_cell, output_keep_prob=self.dropout_keep_rate)
+            self.backward_cell = tf.nn.rnn_cell.DropoutWrapper(self.backward_cell, output_keep_prob=self.dropout_keep_rate)
+
+            # rnn
+            self.outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(
+                self.foward_cell, self.backward_cell, self.emb_all_us, dtype=tf.float32
+            )
+
+        self.output_final = self.outputs[-1]
+
+        # softmax
+        with tf.name_scope('softmax'):
+            self.softmax_w = tf.get_variable('softmax_W', [self.hidden_size * 2, self.class_num])
+            self.softmax_b = tf.get_variable('softmax_b', [self.class_num])
+            self.softmax_pred = tf.matmul(self.output_final, self.softmax_w) + self.softmax_b
+            self.softmax_res = tf.nn.softmax(self.softmax_pred)
+
+        # get max softmax predict result of each relation
+        self.maxres_by_rel = tf.reduce_max(self.softmax_res, 0)
+
+        # class label
+        self.class_label = tf.argmax(self.softmax_res, 1)
+
+        # choose the min loss instance index
+        self.instance_loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.softmax_pred, labels=self.input_labels)
+        self.min_loss_idx = tf.argmin(self.instance_loss, 0)
+
+        # model loss
+        self.model_loss = tf.reduce_mean(self.instance_loss)
+
+        # optimizer
+        if self.learning_rate:
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.model_loss)
+        else:
+            self.optimizer = tf.train.AdamOptimizer().minimize(self.model_loss)
+
+        # # saver
+        # self.saver = tf.train.Saver(tf.all_variables())
 
     def fit(self, session, input_data, dropout_keep_rate):
         feed_dict = {self.input_words: input_data.word,
